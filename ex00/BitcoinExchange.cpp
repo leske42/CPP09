c4c72478f77ad20de52a0f1cf6030e99b3ad35a6
Maria Kamilla Huszar <mhuszar@c4r6p2.42vienna.com>
2024-08-22 22:56:14 +0200

    [ex00] added some error management stuff, still subject to change

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,9 +6,40 @@
 /*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 19:30:07 by mhuszar           #+#    #+#             */
-/*   Updated: 2024/08/22 19:38:44 by mhuszar          ###   ########.fr       */
+/*   Updated: 2024/08/22 22:52:06 by mhuszar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
+void ParseException::DisplayIssue()
+{
+    if (location == DATABASE)
+        std::cerr << "[corrupted database] ";
+    else
+        std::cerr << "[invalid input] ";
+    std::cerr << "On line " << line << ": ";
+    switch (issue)
+    {
+        case INV_SEP:
+            std::cerr << "invalid separation format";
+        case INV_CHAR:
+            std::cerr << "non-numeric character detected";
+        case MON_RNG:
+            std::cerr << "month out of range";
+        case DAY_RNG:
+            std::cerr << "day out of range";
+        case RQ_RNG:
+            std::cerr << "request out of range";
+        case NEG_NUM:
+            std::cerr << "negative request detected";
+        case OTHER:
+            std::cerr << "wrong input format";
+    }
+    std::cerr << std::endl;
+}
+
+void ValidateLine(std::string& line)
+{
+    
+}
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,13 +6,45 @@
 /*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 19:30:09 by mhuszar           #+#    #+#             */
-/*   Updated: 2024/08/22 19:30:37 by mhuszar          ###   ########.fr       */
+/*   Updated: 2024/08/22 22:50:33 by mhuszar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOIN_EX_HPP
 # define BITCOIN_EX_HPP
 
+enum Issue
+{
+    INV_SEP,
+    INV_CHAR,
+    MON_RNG,
+    DAY_RNG,
+    REQ_RNG,
+    NEG_NUM,
+    
+    OTHER
+};
 
+#define DATABASE 0
+#define INPUT 1
+
+class ParseException : public std::exception
+{
+    public:
+
+        ParseException(int loc, unsigned int ln, int type);
+        ~ParseException();
+    
+        void displayIssue();
+
+    private:
+
+        ParseException(void);
+        ParseException(const ParseException& other);
+        ParseException& operator=(const ParseException& other);
+        unsigned int line;
+        int location;
+        int issue;
+};
 
 #endif
